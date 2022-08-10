d28c749e1904b6969abc6967ea758c474b581afd
ramshankar <ramshankar@snakescript.com>
2022-08-10 16:05:57 +0530

    use errorlog

--- config/logging.php
+++ config/logging.php
@@ -18,7 +18,15 @@ return [
     */
 
     'default' => env('LOG_CHANNEL', 'stack'),
-
+    'channels' => [
+        'stack' => [
+            'driver' => 'stack',
+            'channels' => ['single'],
+        ],
+        'single' => [
+            'driver' => 'errorlog',
+            'level' => 'debug',
+        ],
     /*
     |--------------------------------------------------------------------------
     | Deprecations Log Channel
