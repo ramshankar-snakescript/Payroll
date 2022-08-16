3ec0fe0278a388ecd062f574c3893efb8a6d19d4
ramshankar <ramshankar@snakescript.com>
2022-08-16 12:05:27 +0530

    cotroller

--- app/Http/Controllers/DesignationController.php
+++ app/Http/Controllers/DesignationController.php
@@ -12,7 +12,8 @@ class DesignationController extends Controller
 {
     public function index(){
 
-        $department = department::all()->to_array();
+        $department = department::all()
+                    ->to_array();
             $designation = DB::table("designation")
                                 ->join("departments", function($join){
                                     $join->on("departments.id", "=", "designation.dept_id");
